a72fbf71ef5b39866b633173cf640bf955dcee4e
Tom Antonsen <44912232+troldtog@users.noreply.github.com>
2023-04-03 22:04:18 -0500

    Delete some explainer text that we've already talked through.

--- src/routes/(site)/links/+page.svelte
+++ src/routes/(site)/links/+page.svelte
@@ -10,17 +10,6 @@
 <h2>Support</h2>
 
 <div class="py-4">
-    <h3>Hello!</h3>
-
-    <p>
-        I'm not actually sure what to put here. My original idea was to sell subscriptions without
-        Patreon or Ko-Fi taking a cut, but also include Patreon and Ko-Fi links for people who still wanted to use them to send money. Anyway building a subscription- and
-        payment-processing backend takes time and all in all the money wasn't really that different, so I took all the direct sale stuff out at least for now. And then all that was left was the Ko-Fi and Patreon badges, and a few
-        links doesn't really fill a page. If you have a vision for ideas what to do with this page,
-        I can definitey do whatever that vision is though!
-    </p>
-</div>
-<div class="py-3">
     <h3>Here's a logocloud, just cause I wanted to try one</h3>
     <div class="logo-cloud grid-cols-1 xs:!grid-cols-2 lg:!grid-cols-3 gap-1">
         <a class="logo-item" href="https://patreon.com/theSkullSlums">
